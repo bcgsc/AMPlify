43ccb8f6afc7e67ae6798ff0ab2b4d59d3514cb5
figalit <figalitaho@gmail.com>
2019-04-17 11:50:21 -0700

    First commit of pre-release prep.

--- run.py
+++ run.py
@@ -5,15 +5,14 @@ Created on Fri Jan 19 13:48:32 2018
 
 @author: cli
 """
-# In[]:
+import pandas as pd
+from sklearn.externals import joblib
+
 seq = input('Input your sequence: ')
 
-# In[]:
-import pandas as pd
 data = pd.DataFrame()
 data['sequence'] = [seq]
 
-# In[]:
 # one-hot encoding of amino acids
 one_hot = {}
 aa = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
@@ -29,19 +28,12 @@ for i in range(len(data)):
     for j in range(len(data.sequence[i])):
         data.loc[i,'%s_%s' %(j,data.sequence[i][j])] = 1
         
-# In[]:
 X = data.iloc[:,1:]
-
-# In[]:
-from sklearn.externals import joblib
 clf = joblib.load('/projects/btl/cli/AMP-classification/machine_learning/sampled_data_test/physicochemical_properties/proposal/MLP.pkl')
 
-# In[]:
 pred_proba = clf.predict_proba(X)
 pred = clf.predict(X)
 
-
-# In[]:
 print('Your peptide: ' + seq )
 print('Your peptide is ' + pred[0])
 print('Confidence scores for your peptide to be AMP_mature, AMP_precursor and non_AMP: ' + str(pred_proba[0,0]) + ', ' + str(pred_proba[0,1]) + ', ' + str(pred_proba[0,2]))    
